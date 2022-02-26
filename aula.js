2adb1af477b21260e88906c3b4abcf873ea20264
Mayara Alves <1206mkr@gmail.com>
2022-02-25 22:07:26 -0300

    Retomar do bloco de codigo 1.5.2 da apostila 2falando sobre metodo map

---
+++ aula.js
@@ -0,0 +1,38 @@
+//Começando
+
+//int v[10];
+const v1 = []
+console.log(v1.length)
+
+v1[0]=3.4
+v1[10] = 2
+v1[2] = true
+console.log(v1.length)
+
+
+
+/*const n3 = n1+n2
+console.log(n3)*/
+
+/*var idade = 18
+console.log ("oi," + nome)
+if (idade >= 18){
+    //içamento ou hoist =  o escopo não é restrito a bloco
+    var nome = "João"
+console.log ("Parabens, " +nome + ". Você pode dirigir.")
+//printf ("Parabens, %s", nome);
+console.log (`Parabens, ${nome}, Você pode dirigir.`)
+}*/
+
+
+/*const nome = "Mayara"
+const idade = 27
+nome = "Sabrina"
+const a
+
+let a = 2
+let b = 'abc'
+let endereco = 'Rua pi, 78'
+
+var = 2+3
+var d = "abcd"*/
